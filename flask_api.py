bbc5898669681b7396d63de5aab2675408a677ec
cake <c4ke@m.me>
2019-12-16 10:50:03 -0500

    evens odd

--- flask_api.py
+++ flask_api.py
@@ -9,10 +9,22 @@ def hello_world():
     return jsonify({"message": "hello"})
 
 
-@app.route('/evens', methods=["GET"])
+@app.route('/evens', methods=["POST"])
 def evens():
     data = request.get_json()
-    return data
+    for data in data.values():
+        for number in data:
+            if number % 2 == 0:
+                return jsonify({"evens": number})
+
+
+@app.route('/odds', methods=["POST"])
+def odds():
+    data = request.get_json()
+    for data in data.values():
+        for number in data:
+            if number % 3 == 0:
+                return jsonify({"odds": number})
 
 
 if __name__ == "__main__":
