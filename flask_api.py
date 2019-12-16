a04b3c9524b0046da55575d9259c29f1f71409fb
cake <c4ke@m.me>
2019-12-16 10:21:37 -0500

    messge: yoooo

--- flask_api.py
+++ flask_api.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def hello_world():
+    return jsonify({"message": "hello"})
+
+
+@app.route('/evens', methods=["GET"])
+def evens():
+    data = request.get_json()
+    return data
 
 
 if __name__ == "__main__":
